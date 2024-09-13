8804dd4718b94d3146d1c5addda3f490f3318679
Abhishek Rauthan <abhishekrauthan733@gmail.com>
2024-09-13 23:53:38 +0530

    enable cors

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  console.log("url requested", req.originalUrl);
+  return next();
+});
+
 
 // Admin Auth Routes
 const { auth } = require("./admin/auth/auth");
