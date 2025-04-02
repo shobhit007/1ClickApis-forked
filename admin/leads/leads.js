c9859eddb23d27a82c2481b8033281f7cb0a3393
Abhishek Rauthan <abhishekrauthan733@gmail.com>
2025-04-02 20:26:41 +0530

    Update leads.js

--- admin/leads/leads.js
+++ admin/leads/leads.js
@@ -724,6 +724,9 @@ const getLeadsForSalesPanel = async (req, res) => {
       });
     }
 
+    console.log("first", allLeads?.length);
+    allLeads = allLeads?.filter((item) => item.leadType == "manufacturer");
+    console.log("second", allLeads?.length);
     res.status(200).send({ success: true, leads: allLeads });
   } catch (error) {
     res.status(500).send({ success: false, message: error.message });
